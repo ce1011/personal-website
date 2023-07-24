0a7b5fb809cebe45f9343966be3130743d40186d
Paco Au-Yeung <paco20071001@gmail.com>
2023-07-24 06:01:24 -0400

    Update

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -23,13 +23,13 @@ export default function ContactMe(){
                                 <a href="https://www.github.com/ce1011"><FaGithub size="24" className="h-12 text-black"/></a>
 
                             </div>
-                            <div className="flex mt-4 space-x-3 md:mt-6">
-                                <a href="#"
-                                   className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add
-                                    friend</a>
-                                <a href="#"
-                                   className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">Message</a>
-                            </div>
+                            {/*<div className="flex mt-4 space-x-3 md:mt-6">*/}
+                            {/*    <a href="#"*/}
+                            {/*       className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add*/}
+                            {/*        friend</a>*/}
+                            {/*    <a href="#"*/}
+                            {/*       className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">Message</a>*/}
+                            {/*</div>*/}
                         </div>
                     </div>
                 </div>
