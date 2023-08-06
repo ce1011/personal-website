a8d4d215c631002a0bd325695f1c0ed6b688e145
Paco Au-Yeung <paco20071001@gmail.com>
2023-08-06 04:18:38 -0400

    Update

--- src/components/Home/hero.tsx
+++ src/components/Home/hero.tsx
@@ -3,7 +3,7 @@ export default function Hero(){
     return       <section className="hero bg-base-200 text-base-content snap-start min-h-screen">
         <div className="hero-content text-center">
             <div className="max-w-md">
-               <Anime><h1 className="text-5xl font-bold text-transparent bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100 bg-clip-text">Hi there, I am Paco. </h1></Anime>
+               <h1 className="text-5xl font-bold text-transparent bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100 bg-clip-text">Hi there, I am Paco. </h1>
             </div>
         </div>
     </section>
